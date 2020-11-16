6947ff57d249b1169b99abad6cdd9e7c68fc0055
kennylam91 <phamvanlam1991@gmail.com>
2020-11-16 09:08:25 +0700

    change schedule time

--- src/main/java/com/truyenhinh24h/service/ScheduleTaskService.java
+++ src/main/java/com/truyenhinh24h/service/ScheduleTaskService.java
@@ -33,7 +33,7 @@ public class ScheduleTaskService {
 	@Autowired
 	private ChannelService channelService;
 
-	@Scheduled(zone = "GMT+7:00", cron = "0 50 8 ? * *")
+	@Scheduled(zone = "GMT+7:00", cron = "0 10 9 ? * *")
 //	@Scheduled(fixedRate = 60000)
 	public void autoUpdateSchedule() {
 		Calendar cal = Calendar.getInstance();
