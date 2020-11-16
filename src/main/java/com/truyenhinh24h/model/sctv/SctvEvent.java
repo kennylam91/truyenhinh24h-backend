dc91bfcba85a8501651924f08d2131ed17220534
kennylam91 <phamvanlam1991@gmail.com>
2020-11-16 09:06:17 +0700

    add log

--- src/main/java/com/truyenhinh24h/model/sctv/SctvEvent.java
+++ src/main/java/com/truyenhinh24h/model/sctv/SctvEvent.java
@@ -4,7 +4,11 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.ZoneId;
+import java.time.format.DateTimeParseException;
 import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.truyenhinh24h.controller.ScheduleForm;
@@ -14,6 +18,8 @@ import lombok.Data;
 
 @Data
 public class SctvEvent {
+	
+	Logger logger = LoggerFactory.getLogger(SctvEvent.class);
 	
 	@JsonProperty("StartTime")
 	private String StartTime;
@@ -42,14 +48,19 @@ public class SctvEvent {
 	}
 
 	public Schedule mapper(ScheduleForm form) {
-		Schedule schedule = new Schedule();
-		schedule.setProgramName(Name);
-		LocalDateTime startTime = form.getImportDate().atTime(LocalTime.parse(StartTime));
-		LocalDateTime endTime = form.getImportDate().atTime(LocalTime.parse(EndTime));
-		schedule.setStartTime(Date.from(startTime.atZone(ZoneId.of("UTC+07:00")).toInstant()));
-		schedule.setEndTime(Date.from(endTime.atZone(ZoneId.of("UTC+07:00")).toInstant()));
-		schedule.setChannelId(form.getChannelId());
-		schedule.setChannelName(form.getChannelName());
-		return schedule;
+		try {
+			Schedule schedule = new Schedule();
+			schedule.setProgramName(Name);
+			LocalDateTime startTime = form.getImportDate().atTime(LocalTime.parse(StartTime));
+			LocalDateTime endTime = form.getImportDate().atTime(LocalTime.parse(EndTime));
+			schedule.setStartTime(Date.from(startTime.atZone(ZoneId.of("UTC+07:00")).toInstant()));
+			schedule.setEndTime(Date.from(endTime.atZone(ZoneId.of("UTC+07:00")).toInstant()));
+			schedule.setChannelId(form.getChannelId());
+			schedule.setChannelName(form.getChannelName());
+			return schedule;
+		} catch (DateTimeParseException e) {
+			logger.error(e.getMessage() + " " + form.getChannelName() + " " + form.getProgramName());
+			return null;
+		}
 	}
 }
