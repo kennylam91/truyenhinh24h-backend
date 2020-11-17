3ce7cddfba7e65954495982a59a3c3e6c2178d15
kennylam91 <49875994+kennylam91@users.noreply.github.com>
2020-11-17 10:46:08 +0700

    Update ScheduleTaskService.java

--- src/main/java/com/truyenhinh24h/service/ScheduleTaskService.java
+++ src/main/java/com/truyenhinh24h/service/ScheduleTaskService.java
@@ -37,7 +37,7 @@ public class ScheduleTaskService {
 	@Autowired
 	private ChannelService channelService;
 
-	@Scheduled(cron = "0 50 3 * * *")
+	@Scheduled(cron = "0 50 3 ? * * *")
 	public void autoUpdateSchedule() {
 		Calendar cal = Calendar.getInstance();
 		Date now = cal.getTime();
